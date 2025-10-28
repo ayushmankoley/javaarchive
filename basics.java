fde1268bf55cc608fc3f5c13e8ffc013b2e25d08
ayushman <ayushmankoley1@gmail.com>
2025-10-29 02:33:24 +0530

    init

---
+++ basics.java
@@ -0,0 +1,8 @@
+package javaarchive;
+public class basics {
+    public static void main(String[] args){
+        long num = 1000;
+        System.out.println(num);
+    }
+    
+}
---
+++ basics2.java
@@ -0,0 +1,15 @@
+package javaarchive;
+import java.util.Scanner;
+class basics2{
+    public static void main(String[] args) {
+        // Scanner b = new Scanner(System.in);
+        // int num = b.nextInt();
+        // int a = 21;
+        // System.out.println(a);
+        // System.out.println(num);
+        // b.close();
+        // int[] abc = new int[5];
+        // abc[9]=1;
+        // System.out.println(abc[0]);
+    }
+}
